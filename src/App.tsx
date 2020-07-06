ad556607f4ed18a06c5727af471917e1a7d70ff2
Jake LaCombe <jake.lacombe2@gmail.com>
2020-07-06 09:50:48 -0500

    Add Endpoint to Match API Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
 import GoogleMapReact from 'google-map-react';
 import './App.css';
+import { FeatureCollection, Point } from "geojson";
+import { Marker } from "./Marker";
 
 const center = {
  lat: 39.093349,
   lng: -94.580345,
 };
 
-interface Props {
-  text: JSX.Element | string
-  lat: number
-  lng: number
-}
-
-const AnyReactComponent = ({ text, lat, lng }: Props) => <div>{text}</div>;
-
 const App = () => {
+    const [currentMarker, setCurrentMarker] = useState<string>("");
+    const { data } = useQuery<FeatureCollection, 'violations'>('violations', async function() {
+      const response = await fetch('https://data.kcmo.org/resource/ti6s-47nz.geojson');
+      const data = await response.json();
+      return data;
+    });
+
     return (
-      // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyBSRItjUte9j61pdvxwwVHKeY72fbACnZY' }}
           defaultCenter={center}
           defaultZoom={14}
         >
-          <AnyReactComponent
-            lat={39.0915837}
-            lng={-94.8559049}
-            text="My Marker"
-          />
+          {data?.features.map(feature => <Marker
+              address={feature?.properties?.street_address}
+              caseId={feature?.properties?.case_id}
+              onClick={() => {
+                setCurrentMarker(feature?.properties?.case_id || "");
+              }}
+              onClose={() => {
+                setCurrentMarker("");
+              }}
+              isActive={currentMarker === feature?.properties?.case_id}
+              lat={(feature.geometry as Point).coordinates[1]}
+              lng={(feature.geometry as Point).coordinates[0]}
+              />)}   
         </GoogleMapReact>
       </div>
     );
---
+++ src/Marker.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+interface Props {
+  address: string
+  caseId?: string
+  isActive?: boolean,
+  lat: number
+  lng: number
+  onClick: () => unknown
+  onClose: () => unknown
+}
+
+export const Marker = ({ address, caseId, isActive = false, lat, lng, onClick = () => {}, onClose = () => {} }: Props) => {
+  const [data, setData] = useState("");
+
+  const fetchData = async () => {
+    const response = await fetch(`http://kc-covid-violations-api.herokuapp.com/api/${caseId}`);
+    const json = await response.json();
+    return json.data as string;
+  }
+
+  return (
+    <div 
+      onClick={async () => { 
+        onClick();
+        if(!data) {
+          setData(await fetchData());
+        } 
+      }} className={`Marker ${isActive ? 'Active' : ''}`} {...{lat,lng}}>
+        {isActive && <div className="Marker-Text" >
+          <h1 className="Marker-Address">Address <span className="Marker-Close" onClick={(event) => {event.stopPropagation(); onClose()}}>X</span></h1>
+          <p>{address}</p>
+          {!data && <p>Fetching Data...</p>}
+          <div dangerouslySetInnerHTML={{__html: data }} />
+        </div>}
+    </div>
+  );
+}
